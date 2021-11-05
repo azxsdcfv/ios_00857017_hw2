300d93e03d5fa57e0ee6e1bd7b894fe1361c545e
dejes <jack940197@gmail.com>
2021-11-05 22:49:42 +0800

    Initial Commit

---
+++ ios_00857017_hw2/ios_00857017_hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_00857017_hw2App.swift
+//  ios_00857017_hw2
+//
+//  Created by User14 on 2021/11/5.
+//
+
+import SwiftUI
+
+@main
+struct ios_00857017_hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
